54de74662b6800c665c8080be84096c791eef533
Allister McKenzie <allister@crmscience.com>
2021-09-24 18:08:55 -0400

    fix syntax issues

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -14,32 +14,37 @@ contract SimpleBank {
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
-    mapping (address => uint) balances ;
+    mapping (address => uint) private balances;
     
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+    address public owner = msg.sender;
     
     /* Events - publicize actions to external listeners
      */
     
     // Add an argument for this event, an accountAddress
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     /* Functions
      */
+
+    modifier isEnrolled(address _sender) {
+      require(enrolled[_sender]); 
+      _;
+    }
 
     // Fallback function - Called if other functions don't match call or
     // sent ether without data
@@ -52,22 +57,25 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance(address _sender) public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[_sender];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool){
+    function enroll(address _sender) public returns (bool){
       // 1. enroll of the sender of this transaction
+      enrolled[_sender] = true;
+      emit LogEnrolled(_sender);
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit(address _from, uint _amount) public payable isEnrolled(_from) returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
@@ -78,13 +86,16 @@ contract SimpleBank {
       // 4. Emit the appropriate event associated with this function
 
       // 5. return the balance of sndr of this transaction
+      balances[_from] += _amount;
+      emit LogDepositMade(_from, _amount);
+      return balances[_from];
     }
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    function withdraw(address payable _to, uint withdrawAmount) public isEnrolled(_to) returns (uint) {
       // If the sender's balance is at least the amount they want to withdraw,
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
@@ -96,5 +107,11 @@ contract SimpleBank {
       //    sender's balance
 
       // 3. Emit the appropriate event for this message
+      require(getBalance(_to) >= withdrawAmount);
+      //(bool sent, bytes memory data) = _to.call{value: withdrawAmount}("");
+      bool sent = _to.send(withdrawAmount);
+      require(sent, "Failed to withdraw Ether");
+      balances[_to] -= withdrawAmount;
+      emit LogWithdrawal(_to, withdrawAmount, getBalance(_to));
     }
 }
